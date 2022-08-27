83b29dbd4332f87b83f1b42b115b7191b527a1e3
DelmMV <opi588@yandex.ru>
2022-08-27 20:12:59 +0300

    fix

--- controllers/users.js
+++ controllers/users.js
@@ -8,7 +8,11 @@ const BadRequestError = require('../utils/bad-request-error');
 
 module.exports.createUser = (req, res, next) => {
   const {
-    name, about, avatar, email, password,
+    name,
+    about,
+    avatar,
+    email,
+    password,
   } = req.body;
   bcrypt
     .hash(password, 10)
@@ -19,23 +23,24 @@ module.exports.createUser = (req, res, next) => {
       email,
       password: hash,
     }))
-    .then((user) => res.status(CREATE).send({
-      data: {
-        email: user.email,
-        name: user.name,
-        about: user.about,
-        avatar: user.avatar,
-        _id: user._id,
-      },
-    }))
+    .then((user) => res.status(CREATE)
+      .send({
+        data: {
+          email: user.email,
+          name: user.name,
+          about: user.about,
+          avatar: user.avatar,
+          _id: user._id,
+        },
+      }))
     .catch((err) => {
       if (err.code === 11000) {
-         next(new ConflictError('Пользователь уже зарегестрирован'));
+        next(new ConflictError('Пользователь уже зарегестрирован'));
       }
       if (err.name === 'ValidationError') {
-         next(new BadRequestError('Переданы некорректные данные'));
+        next(new BadRequestError('Переданы некорректные данные'));
       }
-       next(err);
+      next(err);
     });
 };
 
@@ -58,25 +63,36 @@ module.exports.getUserById = (req, res, next) => {
       if (err.name === 'CastError') {
         throw new BadRequestError('Передан некорректный id пользователя');
       }
-        next(err);
+      next(err);
     });
 };
 
 module.exports.getMe = (req, res, next) => {
-  User.findById(req.user._id).then((user) => {
-    if (!user) {
-      throw new NotFoundError('Запрашиваемый пользователь не найден');
-    }
-    res.send({ data: user });
-  }).catch(next);
+  User.findById(req.user._id)
+    .then((user) => {
+      if (!user) {
+        throw new NotFoundError('Запрашиваемый пользователь не найден');
+      }
+      res.send({ data: user });
+    })
+    .catch(next);
 };
 
 module.exports.updateUserProfile = (req, res, next) => {
-  const { name, about } = req.body;
+  const {
+    name,
+    about
+  } = req.body;
   User.findByIdAndUpdate(
     req.user._id,
-    { name, about },
-    { new: true, runValidators: true },
+    {
+      name,
+      about
+    },
+    {
+      new: true,
+      runValidators: true
+    },
   )
     .then((user) => {
       if (user) {
@@ -93,7 +109,7 @@ module.exports.updateUserProfile = (req, res, next) => {
         throw new BadRequestError('Передан некорректный id пользователя');
       }
       next(err);
-    })
+    });
 };
 
 module.exports.updateUserAvatar = (req, res, next) => {
@@ -101,7 +117,10 @@ module.exports.updateUserAvatar = (req, res, next) => {
   User.findByIdAndUpdate(
     req.user._id,
     { avatar },
-    { new: true, runValidators: true },
+    {
+      new: true,
+      runValidators: true
+    },
   )
     .then((user) => {
       if (user) {
@@ -118,7 +137,7 @@ module.exports.updateUserAvatar = (req, res, next) => {
         throw new BadRequestError('Передан некорректный id пользователя');
       }
       next(err);
-    })
+    });
 };
 
 module.exports.login = (req, res, next) => {
